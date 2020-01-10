d98b8b83e381312db701f32565c063dc4d5a2fe2
thiagosprestes <thiago.santos.prestes@gmail.com>
2020-01-10 11:06:28 -0300

    Feita validação da api de reclamações

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -4,22 +4,40 @@ import api from '../../services/api'
 
 import Body from '../../components/Body/index'
 
+import Alert from 'react-bootstrap/Alert'
+
 export default function Dashboard() {
     const [posts, setPosts] = useState([])
-    
+    const [load, setLoad] = useState(true)
+    const [error, setError] = useState(false)
+
     useEffect(() => {
         async function loadPosts() {
             const response = await api.get('/reclamacoes')
+            .then((response) => {
+                const data = response.data
 
-            setPosts(response.data)
+                return data
+            })
+            .finally(() => {
+                setLoad(false)
+            }).catch(() => {
+                setError(true)
+            })
+
+            setPosts(response)
         }
 
         loadPosts()
     }, [])
 
     return(
-        <>            
-            <Body name={posts} />        
+        <>          
+            {load && (<Alert variant="warning">Carregando...</Alert>)}
+
+            {!load && error && (<Alert variant="danger">Ocorreu um erro ao carregar</Alert>)}
+
+            {!load && !error && (<Body name={posts} />)}                    
         </>
     )
 }
